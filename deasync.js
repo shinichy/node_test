8009eac5b9078913b918173dad09dca680b0f69f
Shinichi Katayama <shin1.katayama@gmail.com>
2015-11-30 09:40:17 +0900

    Fix to reproduce the problem

--- deasync.js
+++ deasync.js
@@ -6,27 +6,27 @@ var msgpack = require('msgpack');
 
 var server = net.createServer(function(c) {
   c.on('data', function() {
-    c.write(msgpack.pack('hello2\r\n'));
+    c.write(msgpack.pack('foo'));
   });
-  c.write(msgpack.pack('hello\r\n'));
+	c.on('end', function() {
+		console.log('end')
+		server.close()
+	})
+  c.write(msgpack.pack('hello'));
 });
 server.listen(path);
 
 var done = false;
 var client = net.createConnection(path)
-client.on('data', function(data) {
-  console.log('data in client')
-	var response = msgpack.unpack(data)
-  if (response.toString().startsWith('hello')) {
-    client.write(msgpack.pack('hoge\r\n'));
+var stream = new msgpack.Stream(client)
+stream.on('msg', function(msg) {
+  console.log('msg: ' + msg)
+  if (msg === 'hello') {
+    client.write(msgpack.pack('foo'));
     require('deasync').loopWhile(() => !done);
     console.log('done');
-
-    console.log(data.toString());
-  } else if (response.toString().startsWith('hello2')) {
-      console.log('finished!')
-      done = true;
-  } else {
-    console.log('unknown data:' + data)
+		client.end()
+  } else if (msg === 'foo') {
+    done = true;
   }
 });
