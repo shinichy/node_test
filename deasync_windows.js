050aa545620b4bc451c337bec8540f5cea423fb6
Shinichi Katayama <shin1.katayama@gmail.com>
2015-12-01 17:41:43 +0900

    Modify deasync_windows.js

--- deasync_windows.js
+++ deasync_windows.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var net = require('net');
-var path = "\\\\.\\pipe\\foo";
 var port = 8000;
 var host = 'localhost';
 
@@ -11,13 +10,14 @@ var server = net.createServer(function(c) {
     console.log('sending c');
     c.write('c');
   });
+	c.on('end', function() {
+		server.close()
+	});
   c.write('a');
 });
-//server.listen(path);
 server.listen(port, host);
 
 var done = false;
-//var client = net.createConnection(path)
 var client = net.createConnection(port, host)
 client.on('data', function(data) {
   console.log('data on client: ' + data)
@@ -26,9 +26,9 @@ client.on('data', function(data) {
     client.write('b');
     require('deasync').loopWhile(() => !done);
     console.log('done');
+		client.end()
   } else if (data == 'c') {
-      console.log('finished!')
-      done = true;
+    done = true;
   } else {
     console.log('unknown data:' + data)
   }
